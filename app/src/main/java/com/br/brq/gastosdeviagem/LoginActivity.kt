f018f3210a21c1bc0234ae72d0f8f16e9fff5804
Alan OLiveiragit <alan_oliveiral@yahoo.com.br>
2021-05-18 20:38:47 -0300

    Validação Login e senha

--- app/src/main/java/com/br/brq/gastosdeviagem/LoginActivity.kt
+++ app/src/main/java/com/br/brq/gastosdeviagem/LoginActivity.kt
@@ -6,33 +6,61 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     lateinit var btnEntrar: Button
     lateinit var btnCadastreSe: Button
+    lateinit var email: EditText
+    lateinit var senha: EditText
 
-    fun carregarElemento(){
 
+    fun carregarElemento() {
+        email = findViewById(R.id.editEmail)
+        senha = findViewById(R.id.editSenha)
         btnEntrar = findViewById(R.id.buttonEntrar)
         btnCadastreSe = findViewById(R.id.buttonCadastreSe)
+
     }
+
+    fun validarEmail(): Boolean {
+        return email.toString().contains("@") && email.toString().contains(".com")
+    }
+
+    fun validarSenha(): Boolean {
+        return senha.toString().length >= 8
+    }
+
 
     fun carregarEvento() {
         btnEntrar.setOnClickListener() {
+
+
+            if (validarEmail() && validarSenha()) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+
+            } else {
+                Toast.makeText(this, "Email ou Senha Inválido", Toast.LENGTH_LONG)
+                    .show()
+            }
+
+
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
 
-    fun carregarCadastro(){
+    fun carregarCadastro() {
         btnCadastreSe.setOnClickListener() {
+
             val intent = Intent(this, CadastroActivity::class.java)
             startActivity(intent)
         }
     }
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,7 +77,6 @@ class LoginActivity : AppCompatActivity() {
 
 
     }
-
 
 
 }
