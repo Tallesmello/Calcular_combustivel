8619a83b4d629e51a4a34b6cdcdc96cdfb653951
henryfabel <henryfabel@gmail.com>
2021-05-20 02:43:50 -0300

    Começando alógica do Relatório

--- app/src/main/java/com/br/brq/gastosdeviagem/RelatorioActivity.kt
+++ app/src/main/java/com/br/brq/gastosdeviagem/RelatorioActivity.kt
@@ -1,9 +1,23 @@
 package com.br.brq.gastosdeviagem
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.br.brq.gastosdeviagem.R.id.totalResultadoHotel
+import kotlinx.android.synthetic.main.activity_cadastro.*
+import kotlinx.android.synthetic.main.activity_cadastro.view.*
+import kotlinx.android.synthetic.main.activity_hospedagem.*
 
-class RelatorioActivity : AppCompatActivity() {
+abstract class RelatorioActivity : AppCompatActivity(), View.OnClickListener {
+
+    lateinit var btHospedagem: Button
+    lateinit var btCombustivel: Button
+    lateinit var btAlimentacao: Button
+    lateinit var btTotalRelatorio: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_relatorio)
@@ -11,5 +25,25 @@ class RelatorioActivity : AppCompatActivity() {
         if (supportActionBar != null) {
             supportActionBar!!.hide()
         }
+
+        btHospedagem.setOnClickListener {
+
+        }
+
+        totalBtHospedagem()
+
+        fun onClick(view: View) {
+            val id = view.id
+            if (id == R.id.totalResultadoHotel) {
+                totalBtHospedagem()
+            }
+
+        }
+
+    }
+
+    private fun totalBtHospedagem() {
+        val totalBtHospedagem = R.id.totalResultadoHotel
+        println(totalBtHospedagem)
     }
 }
